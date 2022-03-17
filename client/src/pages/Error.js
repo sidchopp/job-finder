facb1bba5bfb679474be705279730223cc3d3988
Sid <sidchopp@gmail.com>
2022-03-17 12:11:08 -0600

    Error page completed with img and wrapper

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom';
+import notFound from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage'
+
 const Error = () => {
   return (
-    <div>
-      <h1>Error...</h1>
-    </div>
+    <Wrapper className='full-page'>
+      <div>
+        <img src={notFound} alt="page not found" className='img' />
+        <h3>Oops!!You have landed in a wrong landing zone..</h3>
+        <p>We can't seem the page you're looking for</p>
+        <Link to="/">Back to Home</Link>
+      </div>
+    </Wrapper>
   )
 }
 export default Error
