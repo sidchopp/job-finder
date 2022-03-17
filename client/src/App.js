3244f0162e77ff584d2c16c08799c5635ac11189
Sid <sidchopp@gmail.com>
2022-03-17 11:56:32 -0600

    Pages structuring done with multiple pages like Register, Error, etc and routes are set for each page in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // Pages
 import Landing from "./pages/Landing";
+import Dashboard from './pages/Dashboard';
+import Error from './pages/Error';
+import Register from './pages/Register';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes >
+        <Route path='/' element={<Dashboard />} />
         <Route path='/landing' element={<Landing />} />
+        <Route path='/register' element={<Register />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/Register.js
@@ -0,0 +1,8 @@
+const Register = () => {
+  return (
+    <div>
+      <h1>Register new user</h1>
+    </div>
+  )
+}
+export default Register
