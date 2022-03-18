bb55606b51e4057ff8cd932cf0038cb7f1faa3d1
Sid <sidchopp@gmail.com>
2022-03-18 09:21:46 -0600

    Register and formrow comps created

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,64 @@
+import { useState, useEffect } from 'react'
+import Logo from '../components/Logo'
+import FormRow from '../components/FormRow'
+import Wrapper from '../assets/wrappers/RegisterPage'
+// global context and useNavigate later
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+}
+// if possible prefer local state
+// global state
+
 const Register = () => {
+  const [values, setValues] = useState(initialState)
+
+  // global context and useNavigate later
+
+  const handleChange = (e) => {
+    console.log(e.target)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(e.target)
+  }
   return (
-    <div>
-      <h1>Register new user</h1>
-    </div>
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        {/* name field */}
+        <FormRow
+          type='text'
+          name='name'
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* email field */}
+        <FormRow
+          type='text'
+          value={values.email}
+          name='email'
+          handleChange={handleChange}
+        />
+        {/* password field */}
+        <FormRow
+          type='text'
+          value={values.password}
+          name='password'
+          handleChange={handleChange}
+        />
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+      </form>
+    </Wrapper>
   )
 }
-export default Register
+export default Register;
