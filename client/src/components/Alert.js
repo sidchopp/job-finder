aade815afe99bc6a89a50ea78a3bcf7ee01e0dd7
Sid <sidchopp@gmail.com>
2022-04-07 14:48:54 -0600

    import alertType and alertText from useAppContext in Alert.js component

--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -1,6 +1,9 @@
+import { useAppContext } from "../context/appContext";
+
 const Alert = () => {
+  const { alertType, alertText } = useAppContext();
   return (
-    <div className="alert alert-danger">Alert</div>
+    <div className={`alert alert-${alertType}`}>{alertText}</div>
   )
 }
 export default Alert;
