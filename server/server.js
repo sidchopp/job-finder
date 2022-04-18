e050863fbd42f29c4907a2dcb3e96e7de988052f
Sid <sidchopp@gmail.com>
2022-04-18 11:19:29 -0600

    errorHandlerMiddleware set up

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 import express from 'express';
+
 const app = express();
 
 //middleware
 import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
 
 
 app.get('/', (req, res) => {
+  throw new Error('error')
   res.send('<h1> Welcome</>')
 })
 
 app.use(notFoundMiddleware);
-
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
