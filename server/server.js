7a57a4d2e924bf6cd5353150895d3dfdfd931615
Sid <sidchopp@gmail.com>
2022-04-18 10:48:29 -0600

    express installed and server is started

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
-console.log('Server running..');
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1> Welcome</>')
+})
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+})
